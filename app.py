e5cb246df606916ef4a193ee42820b0c51a985e8
Julia Poulson <jjpoulson@college.harvard.edu>
2022-12-02 01:39:12 -0500

    upload pictures

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///camerah.db")
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
@@ -42,79 +42,9 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    stocks = {}
     # displays username
     username = db.execute("SELECT username FROM users WHERE id = ?", session["user_id"])[0]["username"]
-    user_purchases = db.execute("SELECT * FROM purchases WHERE user_id = ?", session["user_id"])
-    # creating a dictionary tracking cumulative stocks and num. shares user has currently
-    for row in user_purchases:
-        if row["symbol"] in stocks:
-            stocks[row["symbol"]] += int(row["shares"])
-        else:
-            stocks[row["symbol"]] = int(row["shares"])
-
-    # creating custom table for display
-    table = []
-    # cum_total: total money in stock right now
-    cum_total = 0
-    for symbol, shares in stocks.items():
-        new = []
-        l = lookup(symbol)
-        # stock symbol, name, price, and total
-        new.append(l["symbol"])
-        new.append(l["name"])
-        new.append(usd(l["price"]))
-        new.append(shares)
-        new.append(usd(shares*l["price"]))
-        cum_total += shares*l["price"]
-        if shares != 0:
-            table.append(new)
-
-    # displaying user balance
-    balance = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
-    return render_template("index.html", table=table, total=usd(cum_total), cash=usd(balance), username=username, cash_yass=False)
-
-
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    if request.method == "POST":
-
-        # pre-processing symbol/shares so that incorrect inputs get filtered out
-        symbol = request.form.get("symbol")
-        shares = request.form.get("shares")
-        if not shares.isdigit():
-            return apology("number of shares should be an integer")
-        shares = int(shares)
-
-        l = lookup(symbol)
-        if l is None:
-            return apology("input a valid symbol PLEEK")
-        if shares <= 0:
-            return apology("number of shares should be positive")
-
-        # money transaction (or lack thereof)
-        balance = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
-        cost = shares * l["price"]
-        if cost > balance:
-            return apology("insufficient funds")
-        balance = balance - cost
-
-        # updating database
-        db.execute("UPDATE users SET cash = ? WHERE id = ?", balance, session["user_id"])
-        db.execute("INSERT INTO purchases (user_id, symbol, shares) VALUES(?,?,?)", session["user_id"], l["symbol"], shares)
-        return redirect("/")
-    else:
-        return render_template("buy.html")
-
-
-@app.route("/history")
-@login_required
-def history():
-    # transaction history - only some columns from purchases needed
-    transactions = db.execute("SELECT symbol,shares,date FROM purchases WHERE user_id = ? ", session["user_id"])
-    return render_template("history.html", table=transactions)
-
+    return render_template("index.html", username=username)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -162,22 +92,6 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    if request.method == "POST":
-        symbol = request.form.get("symbol")
-        l = lookup(symbol)
-        if l is None:
-            return apology("input a valid symbol PLEEK")
-        # show price of stock
-        return render_template("quoted.html", symbol=l["symbol"], price=usd(l["price"]), name=l["name"])
-    else:
-        # show form if not post
-        return render_template("quote.html")
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
@@ -202,61 +116,16 @@ def register():
     else:
         return render_template("register.html")
 
-
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    # sets up inventory of user's current stocks
-    stocks = {}
-    user_purchases = db.execute("SELECT * FROM purchases WHERE user_id = ?", session["user_id"])
-    for row in user_purchases:
-        if row["symbol"] in stocks:
-            stocks[row["symbol"]] += int(row["shares"])
-        else:
-            stocks[row["symbol"]] = int(row["shares"])
-
-    symbols = []
-
-    for symbol in stocks:
-        if stocks[symbol] != 0:
-            symbols.append(symbol)
-
-    if request.method == "POST":
-        sym = request.form.get("symbol")
-
-        shares = request.form.get("shares")
-        if not shares.isdigit():
-            return apology("number of shares should be an integer")
-        shares = -1*int(shares)
-        if (shares >= 0):
-            return apology("shares must be positive")
-
-        # checks if number of stocks reasonable
-        print("stocks")
-        print(stocks[sym])
-        if -1 * shares > stocks[sym]:
-            return apology("you're selling more stocks than you have")
-
-        l = lookup(sym)
-
-        # makes changes to database and user balance
-        balance = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
-        change = shares * l["price"]
-        db.execute("UPDATE users SET cash = ? WHERE id = ?", balance - change, session["user_id"])
-        db.execute("INSERT INTO purchases (user_id, symbol, shares) VALUES(?,?,?)", session["user_id"], l["symbol"], shares)
-        return redirect("/")
-
-    else:
-        # if not post: shows form
-        return render_template("sell.html", symbols=symbols)
-
-
 @app.route("/upload", methods=["GET", "POST"])
 def upload():
     """Upload photo"""
     if request.method == "GET":
         return render_template("upload.html")
-
+    else:
+       img = request.files.getlist("file")[0]
+       img.save(os.path.join("./static/photos", img.filename))
+       db.execute("INSERT INTO photos (name, user_id, upvotes) VALUES(?, ?, ?)", img.filename, session["user_id"], 0)
+       return redirect("/")
 
 @app.route("/collage", methods=["GET"])
 def collage():
